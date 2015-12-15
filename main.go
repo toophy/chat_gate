2c9faa039e3adcc4f97433ac717f20d6de71a300
toophy <toophy@vip.qq.com>
2015-12-15 18:00:25 +0800

    以*.epd文件的start_id为依据, 自动顺序分配消息Id, 一个工程的Id不得重复

--- main.go
+++ main.go
@@ -7,17 +7,122 @@ import (
 	"sync"
 )
 
+// 虚拟帐号
+type PlatAccount struct {
+	Id   uint64 // 帐号唯一Id
+	Name string // 帐号名(唯一)
+	Pkey string // 帐号来源平台
+}
+
+// 角色
+type PlatRole struct {
+	Id        uint64 // 角色唯一Id
+	Name      string // 可以为空,表示没有创建角色, 但是登录过
+	Skey      uint64 // 创建角色所在的小区
+	IpAddress string // 最近登录的IP地址
+}
+
 // 主线程
 type MasterThread struct {
 	toogo.Thread
 
 	ChatSvrsLock sync.RWMutex
 	ChatSvrs     map[uint64]*toogo.Session
+
+	LastAccountId    uint64
+	PlatAccountsId   map[uint64]*PlatAccount
+	PlatAccountsName map[string]*PlatAccount
+	PlatRoleId       map[uint64]*PlatRole
+}
+
+func GetPkeyId(pkey string) uint64 {
+	switch pkey {
+	case "tyh":
+		return 1
+	case "6998":
+		return 2
+	case "qqzone":
+		return 3
+	}
+	return 0
+}
+
+func MakeAid(aid uint64, pid uint64) uint64 {
+	return aid<<10 | pid
+}
+
+func MakeRid(aid uint64, pid uint64, sid uint64) uint64 {
+	return sid<<42 | aid<<10 | pid
+}
+
+func MakeRidEx(apid uint64, sid uint64) uint64 {
+	return sid<<42 | apid
+}
+
+func (this *MasterThread) AddAccount(name string, pkey string, skey uint64) bool {
+	if len(name) == 0 || len(pkey) == 0 || len(skey) == 0 {
+		return false
+	}
+
+	fix_acc_name := pkey + "_" + name
+	if v, ok := this.PlatAccountsName[fix_acc_name]; !ok {
+		pa := new(PlatAccount)
+		pa.Id = MakeAid(this.LastAccountId, GetPkeyId(pkey))
+		this.LastAccountId++
+
+		pa.Name = fix_acc_name
+		pa.Pkey = pkey
+		this.PlatAccountsName[fix_acc_name] = pa
+		this.PlatAccountsId[pa.Id] = pa
+
+		pr := new(PlatRole)
+		pr.Id = MakeRidEx(pa.Id, skey)
+		pr.Skey = skey
+		this.PlatRoleId[pr.Id] = pr
+		return true
+	} else {
+
+		rid := MakeRidEx(v.Id, skey)
+		if vr, okr := this.PlatRoleId[rid]; !okr {
+			pr := new(PlatRole)
+			pr.Id = rid
+			pr.Skey = skey
+			this.PlatRoleId[pr.Id] = pr
+		}
+
+		return true
+	}
+
+	return false
+}
+
+func (this *MasterThread) GetRole(name string, pkey string, skey uint64) *PlatRole {
+	if len(name) == 0 || len(pkey) == 0 || len(skey) == 0 {
+		return nil
+	}
+
+	fix_acc_name := pkey + "_" + name
+	if v, ok := this.PlatAccountsName[fix_acc_name]; ok {
+
+		rid := MakeRidEx(v.Id, skey)
+		if v, ok := this.PlatRoleId[rid]; ok {
+			return v
+		}
+	}
+
+	return nil
 }
 
 // 首次运行
 func (this *MasterThread) On_firstRun() {
 	this.ChatSvrs = make(map[uint64]*toogo.Session, 100)
+	this.PlatAccountsId = make(map[uint64]*PlatAccount, 10)
+	this.PlatAccountsName = make(map[string]*PlatAccount, 10)
+	this.PlatRoleId = make(map[uint64]*PlatRole, 10)
+	this.LastAccountId = 1
+
+	this.AddAccount("koko", "tyh", "1")
+	this.AddAccount("bububu", "qqzone", "1")
 }
 
 // 响应线程最先运行
